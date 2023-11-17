a9995d94fda6c96dd44ba8465d955a65c1c9f7f2
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-11-17 15:11:43 +0300

    feat: creates product widget type

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface ProductWidget {
+  id: number;
+  type: 'carbon' | 'plastic bottles' | 'trees';
+  amount: number;
+  action: 'collects' | 'plants' | 'offsets';
+  active: boolean;
+  linked: boolean;
+  selectedColor: 'white' | 'black' | 'blue' | 'green' | 'beige';
+}

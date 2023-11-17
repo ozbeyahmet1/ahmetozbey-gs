97dd5cfeccd67f7a8da4a339b45feba6d1aa4fb2
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-11-17 15:17:45 +0300

    refactor: refactors styling of toggle components

--- src/component/toggle/toggle.stories.tsx
+++ src/component/toggle/toggle.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import '../../styles/globals.scss';
+import "../../styles/globals.scss";
 import { Toggle as ToggleComponent } from "./index";
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
@@ -13,7 +13,6 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-
 } satisfies Meta<typeof ToggleComponent>;
 
 export default meta;
@@ -25,6 +24,3 @@ export const Toggle: Story = {
     defaultChecked: true,
   },
 };
-
-
-

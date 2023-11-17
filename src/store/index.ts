cebf4ccb5e2fb74089fb4a137fb7a6e04df3f7d3
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-11-17 12:31:31 +0300

    build: implements redux for state management

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import exampleReducer from "./slice/exampleSlice";
+export const store = configureStore({
+  reducer: {
+    exampleReducer: exampleReducer,
+  },
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+export const example = (state: RootState) => state.exampleReducer.value;

4bf9de7a4337e7666273f99be32d11e6cb0aa9b6
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-11-17 11:42:53 +0300

    build: creates folder structure with example component

---
+++ src/component/button/button.test.tsx
@@ -0,0 +1,22 @@
+import "@testing-library/jest-dom/extend-expect"; // for extra matchers
+import { fireEvent, render } from "@testing-library/react";
+
+import { Button } from "./Button"; // Adjust the import path based on your project structure
+
+describe("Button component", () => {
+  it("renders with the correct label", () => {
+    const { getByText } = render(<Button label="Click me" />);
+    expect(getByText("Click me")).toBeInTheDocument();
+  });
+
+  it("calls onClick handler when clicked", () => {
+    const onClickMock = jest.fn();
+    const { getByText } = render(<Button label="Click me" onClick={onClickMock} />);
+
+    fireEvent.click(getByText("Click me"));
+
+    expect(onClickMock).toHaveBeenCalledTimes(1);
+  });
+
+  // Add more test cases as needed based on your requirements
+});

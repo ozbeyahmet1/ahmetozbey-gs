9379e9d3b6b769f739a169b33d894b4fc265c2b9
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-11-19 18:23:54 +0300

    feat: adds animations and imports file for scss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import Layout from "@/layout";
 import Homepage from "@/views/homepage";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (

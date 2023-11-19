349d4b951498bbea6841cfc0d3ca3c085567e4bc
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-11-19 18:18:01 +0300

    refactor: reorganize homepage view

--- src/views/homepage/index.tsx
+++ src/views/homepage/index.tsx
@@ -1,5 +1,29 @@
-export interface HomepageProps {}
+import { fetchProducts } from "@/redux/store/slices/productWidgetSlice";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AnyAction } from "redux";
+import Card from "../../component/card";
+import { RootState } from "../../redux";
 
-export default function Homepage(props: HomepageProps) {
-  return <div>Homepage</div>;
+export default function Homepage() {
+  const products = useSelector((state: RootState) => state.productWidgetReducer.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!(products.length > 0)) {
+      dispatch(fetchProducts() as unknown as AnyAction);
+    }
+  }, [dispatch, products]);
+  return (
+    <div className="homepage">
+      <div className="homepage_section">
+        <h1 className="homepage_headline">Per product widgets</h1>
+        <div className="homepage_cardWrapper">
+          {products.map((product, i) => {
+            return <Card {...product} key={i} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }

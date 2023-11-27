42fa01df62f9b5d1d4425208c27f1c5530261727
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-11-27 08:25:34 +0300

    style: refactors sidebar for mobile screens

--- src/layout/sidebar/index.tsx
+++ src/layout/sidebar/index.tsx
@@ -1,7 +1,7 @@
-import Menu from "@/component/menu";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { IoCartOutline, IoHomeOutline } from "react-icons/io5";
+import Menu from "../../component/menu";
 
 export default function Sidebar() {
   const router = useRouter();
@@ -17,12 +17,12 @@ export default function Sidebar() {
         />
       </h1>
       <div className="sidebar_items">
-        <Menu icon={<IoHomeOutline size={25} />} text="Home" isSelected={router.route == "/design"} href="/design" />
+        <Menu icon={<IoHomeOutline size={25} />} text="Home" isSelected={router.route == "/new"} href="/new" />
         <Menu
           icon={<IoCartOutline size={25} />}
           text="My Widgets"
-          isSelected={router.route == "/my-new-widgets"}
-          href="/my-new-widgets"
+          isSelected={router.route == "/new-my-widgets"}
+          href="/new-my-widgets"
         />
       </div>
     </aside>

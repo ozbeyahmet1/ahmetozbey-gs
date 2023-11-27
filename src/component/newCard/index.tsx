a234333633599373ce507cb9a2674a820cfeb93e
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-11-27 08:05:29 +0300

    refactor: refactors newCard componentin terms of styling

--- src/component/newCard/index.tsx
+++ src/component/newCard/index.tsx
@@ -1,11 +1,24 @@
-import { colorConverter, fontColorMatcher } from "@/helpers/utils";
-import { ProductWidget } from "@/types";
 import Image from "next/image";
 import { CSSProperties } from "react";
-import { FaRegUser } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa";
 import { FiEdit } from "react-icons/fi";
+import { colorConverter, fontColorMatcher } from "../../helpers/utils";
+import { ProductWidget } from "../../types";
 
-export default function NewCard({ action, active, amount, id, linked, selectedColor, type }: ProductWidget) {
+export interface NewCardProps {
+  onClick?: () => void;
+  editable?: boolean;
+}
+export default function NewCard({
+  action,
+  active,
+  amount,
+  linked,
+  selectedColor,
+  type,
+  onClick,
+  editable = true,
+}: ProductWidget & NewCardProps) {
   const cardStyle: CSSProperties = {
     background: colorConverter(selectedColor),
     color: fontColorMatcher(selectedColor),
@@ -22,10 +35,14 @@ export default function NewCard({ action, active, amount, id, linked, selectedCo
       />
       <div className="newCard_inline">
         <div className="newCard--top">
-          <FiEdit size={30} />
-          <FaRegUser size={28} />
+          <div className="newCard_linked">{linked && <FaUser size={28} />}</div>
+          {editable && (
+            <div onClick={onClick} className="newCard_edit">
+              <FiEdit size={30} />
+            </div>
+          )}
         </div>
-        <p className="newCard_activity">Active</p>
+        {active ? <p className="newCard_activity--green">Active</p> : <p className="newCard_activity--red">Inactive</p>}
         <p className="newCard_action">This product {action}</p>
         <p className="newCard_amount">
           {amount} {type}
